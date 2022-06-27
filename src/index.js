3a175b8b5252de0a49806aa73dfa68ef4dfecf6c
kotoriefreet <1063377232@qq.com>
2022-06-27 19:54:27 +0800

    jq

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 // // tab栏切换的逻辑
 // console.log('实现tabs标签页的逻辑')
 
+import "./banner.js";
+import "./tabs.js";
 
-
-import './banner.js'
-import './tabs.js'
+import $ from "jquery";
+$("#swiper").css("background-color", "red");
